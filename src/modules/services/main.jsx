9b05c8be884661c0863307ac92860fbc00fa959d
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-11-01 11:54:09 -0500

    data

--- src/modules/services/main.jsx
+++ src/modules/services/main.jsx
@@ -46,17 +46,6 @@ export default function ServicesManagement() {
             status: "inactive",
         },
     ]
-
-    // const [services, setServices] = useState([]);
-    // const [query, setQuery] = React.useState("");
-    // const [currentPage, setCurrentPage] = useState(0);
-    // const itemsPerPage = 5;
-
-    // const filteredServices = services.filter(service =>
-    //     (service.service || '').toLowerCase().includes(query.toLowerCase()) ||
-    //     (service.description || '').toLowerCase().includes(query.toLowerCase())
-    // );
-    // const displayedServices = filteredServices.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
     return (
         <div>
             <TableComponent columns={serviceColumns} users={services} INITIAL_VISIBLE_COLUMNS={initialVisibleColumns} statusOptions={statusOptions} />
