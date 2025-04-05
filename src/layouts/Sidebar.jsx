5bc8ba295dc12f97b37658317c4f529866e66404
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-04-05 08:15:01 -0500

    route

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -15,15 +15,15 @@ import { Link } from "react-router-dom";
 
 const Sidebar = ({ isOpen, onToggle }) => {
     const menuItems = [
-        { icon: <BarChart size={20} />, text: "Dashboard", to: "/dashboard" },
-        { icon: <Home size={20} />, text: "Cabins", to: "/cabins" },
-        { icon: <Home size={20} />, text: "Rooms", to: "/rooms" },
-        { icon: <HotelIcon size={20} />, text: "Services", to: "/services" },
-        { icon: <HelpCircle size={20} />, text: "Plains", to: "/plains" },
-        { icon: <Users size={20} />, text: "Clients", to: "/clients" },
-        { icon: <BookCheck size={20} />, text: "Reservations", to: "/reservations" },
-        { icon: <Users size={20} />, text: "Users", to: "/users" },
-        { icon: <Settings size={20} />, text: "Config", to: "/settings" },
+        { icon: <BarChart size={20} />, text: "Dashboard", to: "/admin/dashboard" },
+        { icon: <Home size={20} />, text: "Cabins", to: "/admin/cabins" },
+        { icon: <Home size={20} />, text: "Rooms", to: "/admin/rooms" },
+        { icon: <HotelIcon size={20} />, text: "Services", to: "/admin/services" },
+        { icon: <HelpCircle size={20} />, text: "Plains", to: "/admin/plains" },
+        { icon: <Users size={20} />, text: "Clients", to: "/admin/clients" },
+        { icon: <BookCheck size={20} />, text: "Reservations", to: "/admin/reservations" },
+        { icon: <Users size={20} />, text: "Users", to: "/admin/users" },
+        { icon: <Settings size={20} />, text: "Config", to: "/admin/settings" },
 
     ];
 
