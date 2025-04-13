ed5d5a4667979ed07db8697105efa4c108ba7824
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-04-13 17:14:28 -0500

    profileIcon

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import ProfileButton from "../utilities/profile/ProfileButton";
 
 export default function NavbarComponent() {
   const navigate = useNavigate();
@@ -27,35 +28,7 @@ export default function NavbarComponent() {
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent as="div" justify="end">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://cdn.jsdelivr.net/gh/alohe/avatars/png/vibrent_22.png"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="gap-2 h-14">
-              <p className="font-semibold">Admin</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger"
-              onClick={handleLogout}
-            >
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+        <ProfileButton user={user.nombre} handleLogout={handleLogout} />
       </NavbarContent>
     </Navbar>
   );
